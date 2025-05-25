93143f26d4133045218d30ecbfbda19e34e97325
KevinCancelo <kevincancelo05@gmail.com>
2025-05-25 20:09:05 +0200

    Test TCIFrame added

---
+++ tfc/src/test/java/com/tfc/tshelpers/pageobjects/POIFrame.java
@@ -0,0 +1,27 @@
+package com.tfc.tshelpers.pageobjects;
+
+import com.microsoft.playwright.Page;
+
+public class POIFrame {
+  Page page;
+
+    private static final String IFRAME = "#mce_0_ifr";
+    private static final String TEXT = "p";
+    private static final String CLOSE_MESSAGE = ".tox-button";
+
+    public POIFrame(Page page){
+
+        this.page = page;
+    }    
+
+    public void closeMessage(){
+
+        page.locator(CLOSE_MESSAGE).click();
+    }
+
+    //Reads the text inside a <p> element within an iframe.
+    public String readText(){
+
+        return page.frameLocator(IFRAME).locator(TEXT).textContent();
+    }
+}
