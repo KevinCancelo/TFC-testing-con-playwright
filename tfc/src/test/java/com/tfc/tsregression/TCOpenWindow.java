f41509a6e3769c3782b90838fb376ee188340a18
KevinCancelo <kevincancelo05@gmail.com>
2025-05-26 22:24:31 +0200

    Test TCOpenWindowFromAnywhere added

--- tfc/src/test/java/com/tfc/tsregression/TCOpenWindow.java
+++ tfc/src/test/java/com/tfc/tsregression/TCOpenWindow.java
@@ -1,5 +1,7 @@
 package com.tfc.tsregression;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import com.microsoft.playwright.Page;
 import com.tfc.tshelpers.base.TCBase;
 import com.tfc.tshelpers.pagecomponents.PCLowBar;
+import com.tfc.tshelpers.pageobjects.POHome;
 
 //Test that verifies the Selenium link opens a new window from differents pages
 public class TCOpenWindow extends TCBase{
@@ -26,6 +29,26 @@ public class TCOpenWindow extends TCBase{
             pcLowBar.clickSeleniumLink();
         });
 
+        //Assert the new window navigated to the expected URL
+        Assertions.assertTrue(newWindowPage.url().contains("elementalselenium.com"));
+    }
+
+    @Tag("user1")
+    @Tag("user2")
+    @Test
+    public void TCOpenWindowFromAnywhere(){
+        getPage().navigate(getHomeUrl());
+
+        //Navigate to a random link
+        POHome poHome = new POHome(getPage());
+        poHome.clickLinkByPosition(new Random().nextInt(41));
+
+        PCLowBar pcLowBar = new PCLowBar(getPage());
+
+        Page newWindowPage = getPage().waitForPopup(() -> {
+            pcLowBar.clickSeleniumLink();
+        });
+        
         //Assert the new window navigated to the expected URL
         Assertions.assertTrue(newWindowPage.url().contains("elementalselenium.com"));
     }
